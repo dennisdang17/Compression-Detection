cb2072c4cf76e772a24654dab2380d788462fddd
Dennis Dang <47399472+dennisdang17@users.noreply.github.com>
2020-04-17 21:57:01 -0700

    Add files via upload

--- server.c
+++ server.c
@@ -7,64 +7,173 @@
 #include <sys/socket.h>       
 #include <netinet/in.h>       
 #include <arpa/inet.h>
-#include <errno.h>
-#include <time.h>
+#include <errno.h> //error reports
+#include <time.h> //clock
+#include <ctype.h>
+#include <json-c/json.h> //for json parsing
+
 #define BUF_SIZE 2000
 #define THRESHOLD 100
 
-
-int main()
+void receive_file(int sockfd)
 {
-    int sockfd;
-    char buffer[BUF_SIZE];
-    char message[25];
-    struct sockaddr_in server_address, client_address;
+    char buffer[BUF_SIZE]; // stores message from client 
 
-    //Creation of socket
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
-    { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
+    FILE *fp = fopen("myconfig.json","w"); //creates a file named myconfig.json in the program directory, 'w' opens for writing purposes
+    if( fp == NULL )
+    {
+        printf("There is an error in opening the file.");
+        return exit(EXIT_FAILURE);
+    }
+
+    while(read(sockfd, buffer, BUF_SIZE) > 0) //reads the file into the buffer
+    {
+        fprintf(fp,"%s", buffer);
+    }
+    printf("Config File received!\n");
+    fclose(fp);
+} 
+
+int main(int argc, char * argv[])
+{
+    int sockfd, connfd, i, packet_id;
+    unsigned int len;
+    char buffer[BUF_SIZE], message[25];
+    int UDPbuffer[BUF_SIZE];
+    FILE * fp;
+    clock_t start_time, end_time;
+    double total_time, low_entropy_time, high_entropy_time;
+    struct sockaddr_in server_address, client_address;
+    struct json_object *parsed_json, *Server_IP_Address, *Source_Port_Number_UDP, *Destination_Port_Number_UDP,
+    *Destination_Port_Number_TCP_Head, *Destination_Port_Number_TCP_Tail, *Port_Number_TCP, 
+    *Size_UDP_Payload, *Inter_Measurement_Time, *Number_UDP_Packets, *TTL_UDP_Packets;
+
+    if(argv[1] == NULL) //check if usage is correct
+    {
+        printf("ERROR!\nUsage is ./server 'my_config_file'\n");
+        return EXIT_FAILURE;
+    }
+    
+
+    //Pre-Probing Phase TCP
+
+
+    //Creation of TCP socket
+    printf("Creating Socket...\n");
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("TCP Socket Creation Failed\n");
+        exit(EXIT_FAILURE);
     }
     else
     {
-        printf("Socket successfully created!\n");
+        printf("TCP Socket Creation Successful\n");
     }
 
-    //zero out the client and server structs
+    //Fill in IP header
     memset(&server_address, 0 , sizeof(server_address));
     memset(&client_address, 0, sizeof(client_address));
-
-    // Filling server and client information.
     server_address.sin_family = AF_INET; // IPv4 
-    server_address.sin_addr.s_addr = inet_addr("192.168.1.28"); //hard coded ip
-    server_address.sin_port = htons(8765); //port numbers
+    server_address.sin_addr.s_addr = inet_addr("192.168.1.30"); //hard coded ip
+    server_address.sin_port = htons(9999); //Port number
 
-    //bind server ip to socket
-    if (bind(sockfd, (const struct sockaddr *)&server_address, sizeof(server_address)) < 0 ) 
+    //Bind TCP socket to port
+    printf("Binding...\n");
+    if ((bind(sockfd, (struct sockaddr *) &server_address, sizeof(server_address))) != 0)
     { 
-        perror("bind failed"); 
+        printf("TCP Socket Bind Failed\n"); 
         exit(EXIT_FAILURE); 
     } 
     else
     {
-        printf("Bind successful\n");
+        printf("TCP Socket Bind Successful\n"); 
     }
 
-
-    //Start of probing phase
-    socklen_t len = sizeof(struct sockaddr_storage);
-    clock_t start_time, end_time;
-    double total_time, low_entropy_time, high_entropy_time;
+    //Listen to receive connections at port
+    printf("Listening...\n");
+    if ((listen(sockfd, 5)) != 0)
+    { 
+        printf("Listening Failed\n"); 
+        exit(EXIT_FAILURE); 
+    } 
     
-    printf("Listening ...\n");
+    //Accept the connection
+    len = sizeof(client_address); 
+    if ((connfd = accept(sockfd, (struct sockaddr *) &client_address, &len)) < 0)
+    { 
+        printf("TCP Connection Failed\n"); 
+        exit(0); 
+    } 
+    else
+    {
+        printf("TCP Connection Established\n"); 
+    }
 
-    //receive low entropy data
+    //calling function to receive file from connection
+    receive_file(connfd);
+
+    //file parsing happens here
+    fp = fopen(argv[1],"r"); //opens the file myconfig.json
+    fread(buffer, BUF_SIZE, 1, fp); //reads files and puts contents inside buffer
+    parsed_json = json_tokener_parse(buffer);
+
+    //Storing the data into the correct variables
+    json_object_object_get_ex(parsed_json, "Server_IP_Address", &Server_IP_Address);
+    json_object_object_get_ex(parsed_json, "Source_Port_Number_UDP", &Source_Port_Number_UDP);
+    json_object_object_get_ex(parsed_json, "Destination_Port_Number_UDP", &Destination_Port_Number_UDP);
+    json_object_object_get_ex(parsed_json, "Destination_Port_Number_TCP_Head", &Destination_Port_Number_TCP_Head);
+    json_object_object_get_ex(parsed_json, "Destination_Port_Number_TCP_Tail", &Destination_Port_Number_TCP_Tail);
+    json_object_object_get_ex(parsed_json, "Port_Number_TCP", &Port_Number_TCP);
+    json_object_object_get_ex(parsed_json, "Size_UDP_Payload", &Size_UDP_Payload);
+    json_object_object_get_ex(parsed_json, "Inter_Measurement_Time", &Inter_Measurement_Time);
+    json_object_object_get_ex(parsed_json, "Number_UDP_Packets", &Number_UDP_Packets);
+    json_object_object_get_ex(parsed_json, "TTL_UDP_Packets", &TTL_UDP_Packets);
+
+    close(sockfd);
+    close(connfd);
+
+
+    //PROBING PHASE//
+
+
+    //Set up the server address for the udp header
+    
+    memset(&UDPbuffer, 0 , sizeof(UDPbuffer));
+    server_address.sin_addr.s_addr = inet_addr(json_object_get_string(Server_IP_Address)); //hard coded ip
+    server_address.sin_port = htons(json_object_get_int(Destination_Port_Number_UDP)); //port number
+
+    //Creation of UDP socket
+    printf("Creating Socket...\n");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
+    { 
+        perror("UDP Socket Creation Failed"); 
+        exit(EXIT_FAILURE); 
+    }
+    else
+    {
+        printf("UDP Socket Creation Successful\n");
+    }
+
+    //Bind the socket to the udp port
+    printf("Binding...\n");
+    if (bind(sockfd, (const struct sockaddr *)&server_address, sizeof(server_address)) < 0 ) 
+    { 
+        perror("UDP Socket Bind Failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    else
+    {
+        printf("UDP Socket Bind Successful\n");
+    }
+
+    //Receive low entropy data
     printf("Receiving low entropy..\n");
     start_time = clock();
-    for(int i = 0; i < 50; i++)//replace 10 with the number of packets
+    for(i = 0; i < json_object_get_int(Number_UDP_Packets); i++)
     {
-        recvfrom(sockfd, buffer, BUF_SIZE, 0, ( struct sockaddr *) &client_address, &len);
+        recvfrom(sockfd, UDPbuffer, BUF_SIZE, 0, ( struct sockaddr *) &client_address, &len); //receive packets into buffer
+        packet_id = (int)(((unsigned)UDPbuffer[1] << 8) | UDPbuffer[0]); //reconstruct the packet id
+        printf("Retrieved Low Entropy Packet Number: %d\n", packet_id);
     }
     end_time = clock();
     total_time  = (((double)end_time) - ((double)start_time)) / ((double)CLOCKS_PER_SEC);
@@ -72,23 +181,24 @@ int main()
     printf("Low Entropy Time: %f\n", low_entropy_time);
 
 
-    printf("Sleeping...\n");
-    sleep(15); //replace with inter time
+    printf("Sleeping...\n"); //Sleeping so packets don't get mixed up
+    sleep(json_object_get_int(Inter_Measurement_Time));
 
 
-    //receive high entropy data
+    //Receive high entropy data
     printf("Receiving high entropy..\n");
-    memset(&buffer, 0, BUF_SIZE);
+    memset(&UDPbuffer, 0, sizeof(UDPbuffer));
     start_time = clock();
-    for(int i = 0; i < 50; i++)//replace 10 with the number of packets
+    for(i = 0; i < json_object_get_int(Number_UDP_Packets); i++)
     {
-        recvfrom(sockfd, buffer, BUF_SIZE, 0, ( struct sockaddr *) &client_address, &len);
+        recvfrom(sockfd, UDPbuffer, BUF_SIZE, 0, ( struct sockaddr *) &client_address, &len); //store packets into buffer
+        packet_id = (((unsigned)UDPbuffer[1] << 8) | UDPbuffer[0]);
+        printf("Retrieved High Entropy Packet Number: %d\n", packet_id);
     }
     end_time = clock();
     total_time  = (((double)end_time) - ((double)start_time)) / ((double)CLOCKS_PER_SEC);
     high_entropy_time = total_time * 1000;
     printf("High Entropy Time: %f\n", high_entropy_time);
-
 
     //Calculate the time and then send back the data
     if((high_entropy_time - low_entropy_time) > THRESHOLD)
@@ -100,11 +210,11 @@ int main()
         strcpy(message, "NO COMPRESSION DETECTED\0");
     }
     
-    
+    //tcp then send message
     
     sendto(sockfd, message, strlen(message), 0, (const struct sockaddr *) &client_address, sizeof(client_address));
     printf("Message sent\n");
-    return 0;
     
-
+    close(sockfd);
+    return 0;
 }
